2e469c773b08f3af035ee9b573d231d6afc47650
sandeepghimire <sandeep.ghimire@student.fh-kiel.de>
2017-10-18 18:44:36 +0200

    components communicationa nd props validation

---
+++ vue2/src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User! : name is : {{names}}</p>
+
+        <button @click= 'changename' > change name </button>
+
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName = 'names'></app-user-detail>
+                <!-- just by binding name ' :name ' we pass the data from parent component to the child components -->
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+    data: function() {
+    return {
+    names: 'sandeep'
+    };
+    },
+    methods: {
+    changename() {
+    this.names = 'akshar'
+    }
+    },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ vue2/src/components/UserDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p> User name : {{myName}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+props: {
+            myName: {
+                type: String,
+                required: true
+            }
+
+}
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
